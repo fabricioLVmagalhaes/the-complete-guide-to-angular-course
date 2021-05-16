f8af9e36cc9da16a92a708763a9b201c0dab07d7
Fabricio Magalhaes <fabricio.l.v.magalhaes@gmail.com>
2021-05-16 20:18:38 -0300

    Providing the Recipe Service Correctly

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -4,7 +4,7 @@ import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
 import { Recipe } from './recipe.model';
 
-@Injectable()
+@Injectable({providedIn: 'root'})
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 

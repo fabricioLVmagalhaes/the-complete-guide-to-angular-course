a4675853c57161ba6e87b7bc2e01c70695467b76
Fabricio Magalhaes <fabricio.l.v.magalhaes@gmail.com>
2021-05-27 20:52:41 -0300

    Login Error Handling

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
@@ -29,39 +29,55 @@ export class AuthService {
           returnSecureToken: true,
         }
       )
-      .pipe(
-        catchError((errorRes) => {
-          let error = 'An unknown error occured!';
-          if (!errorRes.error || !errorRes.error.error)
-            return throwError(error);
-          switch (errorRes.error.error.message) {
-            case 'EMAIL_EXISTS': {
-              error = 'The email address is already in use by another account.';
-              break;
-            }
-            case 'OPERATION_NOT_ALLOWED': {
-              error = 'Password sign-in is disabled for this project.';
-              break;
-            }
-            case 'TOO_MANY_ATTEMPTS_TRY_LATER': {
-              error =
-                'We have blocked all requests from this device due to unusual activity. Try again later.';
-              break;
-            }
-          }
-          return throwError(error);
-        })
-      );
+      .pipe(catchError(this.handleError));
   }
 
   login(email: string, password: string) {
-    return this.http.post<AuthResponseData>(
-      `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${API_KEY}`,
-      {
-        email: email,
-        password: password,
-        returnSecureToken: true,
+    return this.http
+      .post<AuthResponseData>(
+        `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${API_KEY}`,
+        {
+          email: email,
+          password: password,
+          returnSecureToken: true,
+        }
+      )
+      .pipe(catchError(this.handleError));
+  }
+
+  private handleError(errorRes: HttpErrorResponse) {
+    let error = 'An unknown error occured!';
+    if (!errorRes.error || !errorRes.error.error) return throwError(error);
+    switch (errorRes.error.error.message) {
+      case 'EMAIL_EXISTS': {
+        error = 'The email address is already in use by another account.';
+        break;
       }
-    );
+      case 'OPERATION_NOT_ALLOWED': {
+        error = 'Password sign-in is disabled for this project.';
+        break;
+      }
+      case 'TOO_MANY_ATTEMPTS_TRY_LATER': {
+        error =
+          'We have blocked all requests from this device due to unusual activity. Try again later.';
+        break;
+      }
+      case 'EMAIL_NOT_FOUND': {
+        error =
+          'There is no user record corresponding to this identifier. The user may have been deleted.';
+        break;
+      }
+      case 'INVALID_PASSWORD': {
+        error =
+          'The password is invalid or the user does not have a password.';
+        break;
+      }
+      case 'USER_DISABLED': {
+        error =
+          'The user account has been disabled by an administrator.';
+        break;
+      }
+    }
+    return throwError(error);
   }
 }

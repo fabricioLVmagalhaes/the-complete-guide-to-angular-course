75f066d4cd16cda804d6764b0bdce3e92a171a7d
Fabricio Magalhaes <fabricio.l.v.magalhaes@gmail.com>
2021-05-30 16:38:15 -0300

    showing modal using ngIf

--- src/app/shared/alert/alert.component.ts
+++ src/app/shared/alert/alert.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-alert',
@@ -7,4 +7,9 @@ import { Component, Input } from '@angular/core';
 })
 export class AlertComponet {
   @Input() message: string;
+  @Output('close') close = new EventEmitter<void>();
+
+  onClose(){
+    this.close.emit();
+  }
 }

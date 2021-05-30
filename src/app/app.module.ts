5f49f71fe0eb9d66ffe479bfa0c1004c797c0dc0
Fabricio Magalhaes <fabricio.l.v.magalhaes@gmail.com>
2021-05-30 18:10:49 -0300

    The ShoppingList Feature Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,19 +7,12 @@ import { AppComponent } from './app.component';
 import { AuthComponent } from './auth/auth.component';
 import { AuthInterceptorService } from './auth/auth.interceptor.service';
 import { HeaderComponent } from './header/header.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
-import { RecipesComponent } from './recipes/recipes.component';
 import { RecipesModule } from './recipes/recipes.module';
 import { AlertComponet } from './shared/alert/alert.component';
 import { DropdownDirective } from './shared/dropdown.directive';
 import { LoaddingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
 import { PlaceholderDirective } from './shared/placeholder/placeholder.directive';
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingListModule } from './shopping-list/shopping-list.module';
 
 @NgModule({
   declarations: [
@@ -27,8 +20,6 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
     AuthComponent,
     HeaderComponent,
     LoaddingSpinnerComponent,
-    ShoppingListComponent,
-    ShoppingEditComponent,
     DropdownDirective,
     AlertComponet,
     PlaceholderDirective,
@@ -39,7 +30,8 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
     AppRoutingModule,
     ReactiveFormsModule,
     HttpClientModule,
-    RecipesModule
+    RecipesModule,
+    ShoppingListModule,
   ],
   providers: [
     {

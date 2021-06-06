8dc7282fb60340daa4e917fefdbba25e7c89b9a4
Fabricio Magalhaes <fabricio.l.v.magalhaes@gmail.com>
2021-06-06 14:25:21 -0300

    Managing UI State in NgRx

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -2,24 +2,24 @@ import {
   Component,
   ComponentFactoryResolver,
   OnDestroy,
+  OnInit,
   ViewChild,
 } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { Observable, Subscription } from 'rxjs';
-import { finalize } from 'rxjs/operators';
 import { AlertComponet } from '../shared/alert/alert.component';
 import { PlaceholderDirective } from '../shared/placeholder/placeholder.directive';
-import { AuthResponseData, AuthService } from './auth.service';
 import * as fromApp from '../store/app.reducer';
+import { AuthResponseData, AuthService } from './auth.service';
 import * as AuthActions from './store/auth.actions';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
 })
-export class AuthComponent implements OnDestroy {
+export class AuthComponent implements OnInit, OnDestroy {
   isLoginMode = true;
   isLoading = false;
   error: string = null;
@@ -34,6 +34,13 @@ export class AuthComponent implements OnDestroy {
     private componentFactoryResolver: ComponentFactoryResolver,
     private store: Store<fromApp.AppState>
   ) {}
+
+  ngOnInit(): void {
+    this.store.select('auth').subscribe((authState) => {
+      this.isLoading = authState.loading;
+      this.error = authState.authError;
+    });
+  }
 
   onSwitchMode() {
     this.isLoginMode = !this.isLoginMode;
@@ -55,21 +62,23 @@ export class AuthComponent implements OnDestroy {
       authObs = this.authService.signup(email, password);
     }
 
-    authObs
-      .pipe(
-        finalize(() => {
-          this.isLoading = false;
-        })
-      )
-      .subscribe(
-        (response) => {
-          this.router.navigate(['/recipes']);
-        },
-        (error) => {
-          this.error = error;
-          this.showErrorAlert(error);
-        }
-      );
+    this.store.select('auth').subscribe((authState) => {});
+
+    // authObs
+    //   .pipe(
+    //     finalize(() => {
+    //       this.isLoading = false;
+    //     })
+    //   )
+    //   .subscribe(
+    //     (response) => {
+    //       this.router.navigate(['/recipes']);
+    //     },
+    //     (error) => {
+    //       this.error = error;
+    //       this.showErrorAlert(error);
+    //     }
+    //   );
 
     form.reset();
   }
--- src/app/auth/store/auth.reducer.ts
+++ src/app/auth/store/auth.reducer.ts
@@ -3,10 +3,14 @@ import * as AuthActions from './auth.actions';
 
 export interface State {
   user: User;
+  authError: string;
+  loading: boolean;
 }
 
 const initialState = {
   user: null,
+  authError: null,
+  loading: false,
 };
 
 export function authReducer(
@@ -21,15 +25,30 @@ export function authReducer(
         action.payload.token,
         action.payload.expirationDate
       );
-    return {
-      ...state,
-      user
-    }
+      return {
+        ...state,
+        authError: null,
+        user,
+        loading: false,
+      };
     case AuthActions.LOGOUT:
       return {
         ...state,
-        user: null
-      }
+        user: null,
+      };
+    case AuthActions.LOGIN_START:
+      return {
+        ...state,
+        authError: null,
+        loading: true,
+      };
+    case AuthActions.LOGIN_FAIL:
+      return {
+        ...state,
+        user: null,
+        authError: action.payload,
+        loading: false,
+      };
     default:
       return state;
   }

91367f21bcd94377c2c951c82c23d940dc6295d1
Fabricio Magalhaes <fabricio.l.v.magalhaes@gmail.com>
2021-06-06 16:02:08 -0300

    Finishing the Login Effect

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -39,6 +39,7 @@ export class AuthComponent implements OnInit, OnDestroy {
     this.store.select('auth').subscribe((authState) => {
       this.isLoading = authState.loading;
       this.error = authState.authError;
+      if (this.error) this.showErrorAlert(this.error);
     });
   }
 
--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -1,8 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Actions, Effect, ofType } from '@ngrx/effects';
-import { of } from 'rxjs';
-import { catchError, map, switchMap } from 'rxjs/operators';
+import { Router } from '@angular/router';
+import { Actions, createEffect, Effect, ofType } from '@ngrx/effects';
+import { EMPTY, of } from 'rxjs';
+import { catchError, map, mergeMap, switchMap, tap } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import * as AuthActions from './auth.actions';
 
@@ -17,37 +18,93 @@ export interface AuthResponseData {
 
 @Injectable()
 export class AuthEffects {
-  @Effect()
-  authLogin = this.actions$.pipe(
-    ofType(AuthActions.LOGIN_START),
-    switchMap((authData: AuthActions.LoginStart) => {
-      return this.http
-        .post<AuthResponseData>(
-          `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.firebaseApiKey}`,
-          {
-            email: authData.payload.email,
-            password: authData.payload.password,
-            returnSecureToken: true,
-          }
-        )
-        .pipe(
-          map((resData) => {
-            const expirationDate = new Date(
-              new Date().getTime() + +resData.expiresIn * 1000
-            );
-            return of(new AuthActions.Login({
-              email: resData.email,
-              userId: resData.localId,
-              token: resData.idToken,
-              expirationDate
-            }));
-          }),
-          catchError((error) => {
-            return of();
-          })
-        );
-    })
+  authLogin$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(AuthActions.LOGIN_START),
+      switchMap((authData: AuthActions.LoginStart) => {
+        return this.http
+          .post<AuthResponseData>(
+            `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.firebaseApiKey}`,
+            {
+              email: authData.payload.email,
+              password: authData.payload.password,
+              returnSecureToken: true,
+            }
+          )
+          .pipe(
+            map((resData) => {
+              const expirationDate = new Date(
+                new Date().getTime() + +resData.expiresIn * 1000
+              );
+              return {
+                type: AuthActions.LOGIN,
+                payload: {
+                  email: resData.email,
+                  userId: resData.localId,
+                  token: resData.idToken,
+                  expirationDate,
+                },
+              };
+            }),
+            catchError((errorRes) => {
+              let error = 'An unknown error occured!';
+              if (!errorRes.error || !errorRes.error.error)
+                return of(new AuthActions.LoginFail(error));
+              switch (errorRes.error.error.message) {
+                case 'EMAIL_EXISTS': {
+                  error =
+                    'The email address is already in use by another account.';
+                  break;
+                }
+                case 'OPERATION_NOT_ALLOWED': {
+                  error = 'Password sign-in is disabled for this project.';
+                  break;
+                }
+                case 'TOO_MANY_ATTEMPTS_TRY_LATER': {
+                  error =
+                    'We have blocked all requests from this device due to unusual activity. Try again later.';
+                  break;
+                }
+                case 'EMAIL_NOT_FOUND': {
+                  error =
+                    'There is no user record corresponding to this identifier. The user may have been deleted.';
+                  break;
+                }
+                case 'INVALID_PASSWORD': {
+                  error =
+                    'The password is invalid or the user does not have a password.';
+                  break;
+                }
+                case 'USER_DISABLED': {
+                  error =
+                    'The user account has been disabled by an administrator.';
+                  break;
+                }
+              }
+
+              return of(new AuthActions.LoginFail(error));
+            })
+          );
+      })
+    )
   );
 
-  constructor(private actions$: Actions, private http: HttpClient) {}
+  authSuccess = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(AuthActions.LOGIN),
+        tap(() => {
+          this.router.navigate(['/']);
+        })
+      ),
+    {
+      dispatch: false,
+    }
+  );
+
+  constructor(
+    private actions$: Actions,
+    private http: HttpClient,
+    private router: Router
+  ) {}
 }
